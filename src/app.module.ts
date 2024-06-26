ad35efd51384c218374125f5a9fba0012f1ac670
Rafaela Lima <ral@icomp.ufam.edu.br>
2024-06-26 16:21:33 -0400

    feat: spots crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { SpotsModule } from './spots/spots.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, PrismaModule, SpotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
